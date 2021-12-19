b0773de47360c034954f716416711fc3d2481de5
vivi1 <viviendupont21@gmail.com>
2021-12-19 17:50:14 +0100

    Wang + steinfojoo

--- Fichiers R/Exo1.R
+++ Fichiers R/Exo1.R
@@ -10,8 +10,16 @@ tumeur <- read_table2("C:/Users/vivi1/Desktop/Ensai 3A/Projet Pharma/Pharmaco/Ex
 
 tumeur$ID <- as.factor(tumeur$ID) # Codage en variable qualitative
 tumeur$BLIVER <- as.logical(tumeur$BLIVER) # Codage en indicatrice
+tumeur$BSLD <- tumeur$SLD
 
-covar_ind <- tumeur[which(duplicated(tumeur$ID) == F), 4:6] # Je récupère les covariables pour faciliter leur description
+for (i in levels(tumeur$ID)) {
+  tumeur$BSLD[which(tumeur$ID == i)] <- tumeur$BSLD[which(tumeur$ID == i)][1]
+}
+
+
+covar_ind <- tumeur[which(duplicated(tumeur$ID) == F), 3:6] # Je récupère les covariables pour faciliter leur description
+names(covar_ind)[1] <- "BSLD"
+
 
 Dose <- data.frame(
   ID = rep(tumeur$ID[which(duplicated(tumeur$ID) == F)], 25),
@@ -20,7 +28,8 @@ Dose <- data.frame(
   SLD = NA,
   AGE = rep(tumeur$AGE[which(duplicated(tumeur$ID) == F)],25),
   BLIVER = rep(tumeur$BLIVER[which(duplicated(tumeur$ID) == F)],25),
-  BHBG = rep(tumeur$BHBG[which(duplicated(tumeur$ID) == F)],25)
+  BHBG = rep(tumeur$BHBG[which(duplicated(tumeur$ID) == F)],25),
+  BSLD = rep(covar_ind$BSLD,25)
 ) # Création d'un tableau de Doses administrées. Une Dose en 0 puis une tous les 21j jusqu'à ce que le temps t+21 d'administration soit
 # supérieur au temps de dernière mesure (car dans ce cas on a aucune info).
 
@@ -33,7 +42,7 @@ for (j in 1:1225) {
   }
 } 
 
-Dose <- Dose[which(Dose$suppr == 0), -8]
+Dose <- Dose[which(Dose$suppr == 0), -9]
 
 tumeur_Dose <- merge(Dose, tumeur, all.x = T, all.y = T)
 
@@ -96,18 +105,59 @@ for (i in 2:130) {
 }
 
 
-# Pour g ------------------------------------------------------------------
+
+
+# Pour Wang ---------------------------------------------------------------
+# g -----------------------------------------------------------------------
+
+
 
 library(nlme)
-reg_param_croiss <- lme(SLD~time, data = tumeur[which(tumeur$indice==1),], random = ~ 1 + time | ID, method = "ML")
-summary(reg_param_croiss) # g estimé à 0.09255
+reg_param_croiss_Wang <- lme(SLD~time, data = tumeur[which(tumeur$indice==1),], random = ~ 1 + time | ID, method = "ML")
+summary(reg_param_croiss_Wang) # g estimé à 0.09255 et son écart-type à 0.045398
+g_Wang <- 0.09255
+g_sd_Wang <- 0.045398
 
 
+# d -----------------------------------------------------------------------
 
-reg_param_decroiss <- lme(SLD~time, data = tumeur[which(tumeur$indice==0),], random = ~ 1 + time | ID, method = "ML")
-summary(reg_param_decroiss)  # Ne pas oublier qu'on est dans une exponentielle
+reg_param_decroiss_Wang <- lme(SLD~time, data = tumeur[which(tumeur$indice==0),], random = ~ 1 + time | ID, method = "ML")
+summary(reg_param_decroiss_Wang)  # Ne pas oublier qu'on est dans une exponentielle
 log(0.23689) # d = 1.440159 
 -exp(log(0.23689))
+d_Wang <- 1.440159/BSLD
+d_sd_Wang <- 0.060186/0.23689*d_Wang
+
+
+# Pour Stein-Fojo --------------------------------------------------------
+# SLD(t) = BSLD(exp(-d*t) + exp(g*t) - 1)
+
+
+# Pour g ------------------------------------------------------------------
+
+reg_param_croiss_SteinFojo <- lme(SLD/BSLD~time - 1, data = tumeur[which(tumeur$indice==1),], random = ~ 1 + time | ID, method = "ML")
+summary(reg_param_croiss_SteinFojo)
+
+# exp(g*t) -1 = 0.01384
+# g*t = ln(1.01384)
+
+g_SteinFojo <- log(1.01384)
+g_sd_SteinFojo <- 0.001322597/0.01384131*g_SteinFojo
+
+
+# Pour d ------------------------------------------------------------------
+
+reg_param_decroiss_SteinFojo <- lme(SLD/BSLD~time , data = tumeur[which(tumeur$indice==0),], random = ~ 1 + time | ID, method = "ML",
+                                    control =  lmeControl(maxIter = 500, niterEM = 500, msMaxEval = 500,msMaxIter = 500))
+summary(reg_param_decroiss_SteinFojo)
+
+# log(exp(-d*t)-1) = -0.004035
+-log(exp(-0.004035) + 1)
+
+
+
+
+
 
 
 
